77df6a46fc70fc1ebf8afd5369ba01f3d2d85af7
Geilen <M.C.W.Geilen@tue.nl>
2019-10-16 08:24:44 +0200

    first console version in VS

--- ParetoCalculatorConsole/pcconsole.h
+++ ParetoCalculatorConsole/pcconsole.h
@@ -1,7 +1,42 @@
+//
+// The MIT License
+//
+// Copyright (c) 2008-2019 Eindhoven University of Technology
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+//
+
+//
+// Author: Marc Geilen, e-mail: m.c.w.geilen@tue.nl
+// Electronic Systems Group (ES), Faculty of Electrical Engineering,
+// Eindhoven University of Technology
+//
+// Description:
+// Implementation of the PCConsole class, implementing a simple console interface to the calculator
+//
+
 #pragma once
 
-
 #include <iostream>
+#include <string>
+
+#include <calculator.h>
 
 namespace Pareto {
 
@@ -9,6 +44,16 @@ namespace Pareto {
 	{
 	public:
 		PCConsole(std::ostream& outstr);
+
+		void LoadFile(std::wstring filename);
+		void LoadQuantityTypes();
+		void LoadConfigurationSpaces();
+		void LoadConfigurationSets();
+		void LoadOperations();
+
+	private:
+		std::ostream& _outstr;
+		ParetoCalculator _pc;
 	};
 
 
