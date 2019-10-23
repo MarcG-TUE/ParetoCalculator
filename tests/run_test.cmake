d695161778b4f4478a6059ada489e2386a98eea0
Marc Geilen <m.c.w.geilen@tue.nl>
2019-10-23 10:56:59 +0200

    test setup

--- tests/run_test.cmake
+++ tests/run_test.cmake
@@ -3,24 +3,24 @@
 # test_cmd is the command to run with all its arguments
 if( NOT test_cmd )
     message( FATAL_ERROR "Variable test_cmd not defined" )
-if( NOT test_cmd )
+endif()
 
 # output_blessed contains the name of the "blessed" output file
 if( NOT output_blessed )
     message( FATAL_ERROR "Variable output_blessed not defined" )
-if( NOT output_blessed )
+endif()
 
 # output_test contains the name of the output file the test_cmd will produce
 if( NOT output_test )
     message( FATAL_ERROR "Variable output_test not defined" )
-if( NOT output_test )
+endif()
 
-execute_command(
+execute_process(
     COMMAND ${test_cmd}
     COMMAND ${CMAKE_COMMAND} -E compare_files ${output_blessed} ${output_test}
     RESULT_VARIABLE test_not_successful
-    OUTPUT_QUIET
-    ERROR_QUIET
+#    OUTPUT_QUIET
+#    ERROR_QUIET
 )
 
 if( test_not_successful )
